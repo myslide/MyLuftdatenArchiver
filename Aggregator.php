2075e4d1a6a01dfd8c87b31b79cb781103c79aef
myslide <jess@schied.info>
2023-10-31 22:58:46 +0100

    Aggregator takes account on archived files of the past years now.

--- Aggregator.php
+++ Aggregator.php
@@ -4,7 +4,7 @@
  * The Base class to pull data from archive.luftdaten.info.
  * It stores the date specific csv-files into a given directory.
  * @author mysli
- * @version 20180118
+ * @version 20231031
  */
 class Aggregator
 {
@@ -25,23 +25,45 @@ class Aggregator
     protected $badFiles;
 
     public function aggregate(DateTime $startdate, DateTime $enddate, $targetDirectory)
-    {
-        $end = $enddate->diff($startdate)->days;
+    {           //all previous years are in a archived folder. \YYYY\YYYY-mm-dd\..
+
+                $currentYear = date("Y");
+                $startYear=$nstartYear=$startdate->format("Y");
+                $yearSpan= $currentYear-$startYear;
+                $archiveFolder=$yearSpan>0 ? $startYear . "/":"";
+                //files from 2 years before are zipped
+                $gz= $yearSpan>1 ? ".gz":"";
+                $end = $enddate->diff($startdate)->days;
         for ($i = 0; $i < $end; $i ++) {
             try {
                 $aggregateDate = $startdate->add($this->interval)->format(Aggregator::DatePattern);
                 $csvfile = $aggregateDate . $this->filebody;
-                $url = $this->webPage . $aggregateDate . "/" . $csvfile;
+
+                $url = $this->webPage . $archiveFolder . $aggregateDate . "/" . $csvfile.$gz;
                 
                 $targetFile = $targetDirectory . $csvfile;
                 if (file_exists($targetFile)) {
-                    echo $targetFile . " just exist.\n";
+                    echo $targetFile . " already exist.\n";
                 } else if (@copy($url, $targetFile)) { // the '@' supresses the warnings
                     $this->files[] = $csvfile;
-                    echo "Got file " . $csvfile . "\n";
+                    echo "Got file " . $csvfile . $gz."\n";
                 } else {
-                    $this->badFiles[] = $csvfile;
                     echo $url . " does not exist in archive.\n";
+                    $nstartYear=$nstartYear+1;
+                    $yearSpan= $currentYear-$nstartYear;
+                    $narchiveFolder = $yearSpan>0 ? $nstartYear . "/":"";
+                    $ngz= $yearSpan>1 ? ".gz":"";
+                    //check
+                    $nurl = $this->webPage . $narchiveFolder . $aggregateDate . "/" . $csvfile. $ngz;
+                    if (@copy($nurl, $targetFile)){
+                         $archiveFolder = $narchiveFolder;
+                         $gz=$ngz;
+                         $this->files[] = $csvfile;
+                    echo "Got file " . $csvfile . $gz . "\n";
+                    } else {                     
+                    $nstartYear=$startYear;   
+                    $this->badFiles[] = $csvfile;
+                    echo $nurl . " does not exist in archive also. Check the sensor ID.\n";}
                 }
             } catch (Exception $ex) {}
         }
